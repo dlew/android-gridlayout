bff28527db82bbbdbdfebf7d16b6129ed0a0ce9a
Daniel Lew <danlew42@gmail.com>
2012-01-09 18:32:14 -0600

    Added basic sample project

---
+++ samples/src/com/gridlayout/samples/GridLayoutSamplesActivity.java
@@ -0,0 +1,30 @@
+package com.gridlayout.samples;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class GridLayoutSamplesActivity extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, SAMPLES));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = SAMPLES[position];
+        if (item.equals(SAMPLE_SIMPLE)) {
+            startActivity(new Intent(this, SimpleGridLayoutActivity.class));
+        }
+    }
+
+    private static final String SAMPLE_SIMPLE = "Simple GridLayout Activity";
+
+    private static final String[] SAMPLES = new String[] { SAMPLE_SIMPLE };
+}
---
+++ samples/src/com/gridlayout/samples/SimpleGridLayoutActivity.java
@@ -0,0 +1,19 @@
+package com.gridlayout.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Demonstrates a simple GridLayout created from XML.
+ * 
+ * Uses the same sample code as from http://android-developers.blogspot.com/2011/11/new-layout-widgets-space-and-gridlayout.html
+ */
+public class SimpleGridLayoutActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_gridlayout);
+    }
+
+}
